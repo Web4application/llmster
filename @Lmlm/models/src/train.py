a65f272117febe37dffde997e2292dd7ceeeeedc
Seriki yakub <167559384+Web4application@users.noreply.github.com>
2026-05-11 07:27:27 +0100

    Create train.py

---
+++ @Lmlm/models/src/train.py
@@ -0,0 +1,7 @@
+python3 {sgd/adam}_torch.py \
+    --dataset ["prototypes"/"fmnist"/"bfmnist"/"cifar10tl"/"bcifar10tl"/"imagenettetl"/"bimagenettetl"/"cifar100tl"/"bcifar100tl"] \
+    --layers [str] \
+    --lr [float] \
+    --seed [int] \
+    --n-runs [int] \
+    --device [int] \
